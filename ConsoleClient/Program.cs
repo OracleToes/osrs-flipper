e644fd6e7b2fe3f483a0061e07ba73b95a5ecc5f
Jasper <japsu.honkasalo@gmail.com>
2024-02-12 17:54:57 +0200

    Remove automatic updating

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -1,6 +1,4 @@
-﻿using System.Reflection;
-using AutoUpdateViaGitHubRelease;
-using OsrsFlipper;
+﻿using OsrsFlipper;
 
 namespace ConsoleClient;
 
@@ -15,8 +13,6 @@ internal static class Program
     private static async Task Main(string[] args)
     {
         Logger.Info("Starting...");
-
-        CheckForUpdates();
 
         using Flipper flipper = await Flipper.Create();
 
@@ -38,30 +34,5 @@ internal static class Program
 
             await Task.Delay(TimeSpan.FromSeconds(REFRESH_FLIPPER_DATA_INTERVAL));
         }
-    }
-
-
-    private static void CheckForUpdates()
-    {
-        Logger.Info("Checking for updates...");
-        Assembly assembly = Assembly.GetExecutingAssembly();
-        string tempDir = Path.Combine(Path.GetTempPath(), "OsrsFlipper");
-        Directory.CreateDirectory(tempDir);
-        string updateArchive = Path.Combine(tempDir, "update.zip");
-        Version? version = assembly.GetName().Version;
-        Task<bool> updateTask = UpdateTools.CheckDownloadNewVersionAsync("japsuu", "osrs-flipper", version, updateArchive);
-        bool updateAvailable = updateTask.Result;
-
-        if (!updateAvailable)
-            return;
-
-        Console.Write("Update? (Y/N)");
-        if (ConsoleKey.Y != Console.ReadKey().Key)
-            return;
-
-        string installer = Path.Combine(tempDir, UpdateTools.DownloadExtractInstallerToAsync(tempDir).Result);
-        string? destinationDir = Path.GetDirectoryName(assembly.Location);
-        UpdateTools.StartInstall(installer, updateArchive, destinationDir); // The application needs to be closed before it can be updated.
-        Environment.Exit(0);
     }
 }
