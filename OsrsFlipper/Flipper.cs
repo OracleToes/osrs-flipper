031fac97a21aceec777f25d76204f77ab1003f49
Jasper <japsu.honkasalo@gmail.com>
2024-02-25 17:59:21 +0200

    Fix graph images getting deleted on some platforms

--- OsrsFlipper/Flipper.cs
+++ OsrsFlipper/Flipper.cs
@@ -185,7 +185,7 @@ public sealed class Flipper : IDisposable
             }
             
             // Check if the item passes all pruning filters.
-            // DEBUG-TEST: if (entry.Item.Id != 2)
+            if (entry.Item.Id != 2)
             if (!_filterCollection.PassesPruneTest(entry))
                 continue;
             itemsPassedPruneCount++;
@@ -196,7 +196,7 @@ public sealed class Flipper : IDisposable
                 continue;
 
             // Check if the item passes all flip filters.
-            // DEBUG-TEST: if (entry.Item.Id != 2)
+            if (entry.Item.Id != 2)
             if (!_filterCollection.PassesFlipTest(entry, history5Min))
                 continue;
             
